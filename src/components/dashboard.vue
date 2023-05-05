e8e13057aab6164c1d9942ea929465e6db7d1349
ithiel883 <ajuka.zephiniah@gmail.com>
2023-05-05 14:07:14 +0100

    initial commit

---
+++ src/components/dashboard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const passcode = ref('')
+
+const handlePass = () => {
+  if(passcode.value === 'turndasix'){
+    window.location.href = 'https://nine.company.site/'
+  }else {
+
+  }
+}
+
+</script>
+
+<template>
+<div class="h-screen">
+    <h3 class="font-extrabold	 text-xl ml-5">turndasix.</h3>
+    <div class="justify-center flex bg-black items-center h-screen">
+    <div class="img-wrapper flex flex-col justify-center items-center">
+      <img src="../assets/blackgif.gif" class="h-full w-full"/>
+      <div class="flex flex-col justify-between w-full px-10">
+        <input v-model="passcode" type="text" placeholder="enter passcode" class="w-full bg-black border-white border text-white mb-5 p-4 rounded"/>
+        <button @click="handlePass" class="text-white  border border-white p-2 cursor-pointer">
+          enter
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+<style scoped>
+.img-wrapper {
+  height: 400px;
+  width: 400px;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+}
+

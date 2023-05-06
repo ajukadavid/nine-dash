ea112c99fd7cc1b40bce311b6e1cfb7e41f90b97
ithiel883 <ajuka.zephiniah@gmail.com>
2023-05-06 10:50:18 +0100

    removed gif bg and changed cta text

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -24,6 +24,7 @@ const handlePass = () => {
 
   }
   isWrongCode.value = true
+  return
 }
 
 
@@ -31,21 +32,20 @@ const handlePass = () => {
 
 <template>
   <div class="h-screen">
-    <h3 class="font-extrabold	 text-xl ml-5">turndasix.</h3>
     <div class="justify-center flex bg-black items-center h-screen overflow-hidden">
       <div class="img-wrapper flex flex-col justify-center items-center">
         <div class="">
           <div class="ml-20 mb-10 lg:ml-24 leading-loose" v-if="isWrongCode">
             <span class="text-red-700 text-4xl" >you not welcome</span>
           </div>
-          <img v-else src="../assets/blackgif.gif" class="h-full w-full logo" />
+          <img v-else src="../assets/updated.gif" class="h-full w-full logo" />
           
         </div>
         <div class="flex flex-col justify-between w-full px-10">
           <input v-model="passcode"  type="password" placeholder="enter passcode"
-            class="w-full bg-black border-white border text-white mb-5 p-4 rounded" />
-          <button @click="handlePass" class="text-white  border border-white p-2 cursor-pointer">
-            enter
+            class="w-full bg-black border-red-400 border text-white mb-5 p-4 rounded" />
+          <button @click="handlePass" class="text-white text-3xl    p-2 cursor-pointer">
+            🔞
           </button>
         </div>
       </div>

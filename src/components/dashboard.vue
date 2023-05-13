1dda2c5f47036370e5dd8709eddeea82c4d60b2c
ithiel883 <ajuka.zephiniah@gmail.com>
2023-05-13 21:58:11 +0100

    design changes

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,36 +1,35 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-// import gsap from 'gsap'
-import { sendEmail } from '../composables/email-service'
+import gsap from 'gsap'
+// import { sendEmail } from '../composables/email-service'
 
 const passcode = ref('')
 const isWrongCode = ref(false)
 
 const handlePass = () => {
-  sendEmail()
-//   if (passcode.value === 'turndasix') {
-//     window.location.href = 'https://nine.company.site/'
-//   } else {
-//     gsap.from(".logo", { 
-//       x: -60,
-//       fill: 'blue',
-//       repeat: 3,
-//       yoyo: true,
-//       ease: "rough({ strength: 1, points: 20, template: none.out, taper: none, randomize: true, clamp: false })",
-//       onComplete: function(){
-//         gsap.set('.logo', {
-//           x: 0
-//         })
-//       }
-//      })
+  if (passcode.value === 'turndasix') {
+    window.location.href = 'https://nine.company.site/'
+  } else {
+    gsap.from(".logo", { 
+      x: -60,
+      fill: 'blue',
+      repeat: 3,
+      yoyo: true,
+      ease: "rough({ strength: 1, points: 20, template: none.out, taper: none, randomize: true, clamp: false })",
+      onComplete: function(){
+        gsap.set('.logo', {
+          x: 0
+        })
+      }
+     })
 
-//   }
-//   isWrongCode.value = true
-//   passcode.value = ''
-//   setTimeout(() => {
-//     isWrongCode.value = false
-// }, 9000);
-//   return
+  }
+  isWrongCode.value = true
+  passcode.value = ''
+  setTimeout(() => {
+    isWrongCode.value = false
+}, 9000);
+  return
 }
 
 
@@ -40,13 +39,13 @@ const handlePass = () => {
   <div class="h-screen">
     <div class="justify-center flex bg-black items-center h-screen overflow-hidden">
       <div class="img-wrapper flex flex-col justify-center items-center">
-        <div class="flex">
-          <div class="mx-auto animate-bounce" v-if="isWrongCode">
-            <p class="text-red-700 text-4xl ml-28 leading-loose" >you not welcome</p>
+        <div class="flex h-[350px] items-center justify-center">
+          <div class="mx-auto animate-bounce w-full" v-if="isWrongCode">
+            <p class="text-red-700 text-6xl leading-loose" >keep off</p>
           </div>
           <img v-else src="../assets/updated.gif" class="h-full w-full logo" />
         </div>
-        <div class="flex items-center justify-center w-full px-10">
+        <div class="flex flex-col items-center justify-center w-full px-10">
           <input v-model="passcode"  type="password" placeholder="enter passcode"
             class="w-full bg-black border-red-400 border text-white mb-5 p-4 rounded" />
           <button @click="handlePass" class="text-white text-2xl    p-2 cursor-pointer">
--- src/composables/email-service.ts
+++ src/composables/email-service.ts
@@ -1,16 +1,16 @@
-import emailjs from '@emailjs/browser';
+// import emailjs from '@emailjs/browser';
 
-const templateParams = {
-    name: 'James',
-    notes: 'Check this out!',
-    recipient: 'silvahstix@gmail.com'
-};
+// const templateParams = {
+//     name: 'James',
+//     notes: 'Check this out!',
+//     recipient: 'silvahstix@gmail.com'
+// };
 
-export const sendEmail = () => {
-    emailjs.send('service_6e36k6q','template_qx7fykb', templateParams, 'W5kexZDNe7KDfnQ7F')
-	.then((response) => {
-	   console.log('SUCCESS!', response.status, response.text);
-	}, (err) => {
-	   console.log('FAILED...', err);
-	});
-}
+// export const sendEmail = () => {
+//     emailjs.send('service_6e36k6q','template_qx7fykb', templateParams, 'W5kexZDNe7KDfnQ7F')
+// 	.then((response) => {
+// 	   console.log('SUCCESS!', response.status, response.text);
+// 	}, (err) => {
+// 	   console.log('FAILED...', err);
+// 	});
+// }

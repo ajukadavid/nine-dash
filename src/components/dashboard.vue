b62a25d5c7dc906ef70a0d0d4e36ad73c2dba877
ithiel883 <ajuka.zephiniah@gmail.com>
2023-05-21 21:33:41 +0100

    text size change

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -98,7 +98,7 @@ const handlePass = () => {
         <div class="flex flex-col items-center justify-center w-full px-5">
           <div class="flex justify-between items-center" v-if="!isEmail">
             <input v-model="passcode"  type="password" placeholder="enter passcode"
-            class="w-3/4 h-[50px] bg-black border-red-400 border text-white mx-6 mb-5 p-4 rounded" />
+            class="w-3/4 h-[50px] bg-black border-red-400 border text-xs text-white mx-6 mb-5 p-4 rounded" />
           <button v-if="!isEmail && !isWrongCode" @click="handlePass" class="text-white text-xl mb-4 px-2 cursor-pointer">
             JOIN
           </button>

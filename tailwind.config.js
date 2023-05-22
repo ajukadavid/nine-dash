405642610ab9d82318ca3a0b2e626c0a633f97c3
ithiel883 <ajuka.zephiniah@gmail.com>
2023-05-22 06:21:55 +0100

    updated font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        joystix: ['joystix', 'monspace-regular']
+        pricedow: ['pricedow', 'monspace-regular']
       }
     },
   },

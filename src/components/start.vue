d6accfe38b88da9eb68eaab8dce82b8f2183fae6
ithiel883 <ajuka.zephiniah@gmail.com>
2023-06-30 03:45:42 +0100

    desktop mode

--- src/components/start.vue
+++ src/components/start.vue
@@ -3,20 +3,26 @@
 </script>
 
 <template>
-    <div class="box h-screen w-full flex items-center justify-center">
+    <div class="box h-screen w-full gap-8 bg-black flex flex-col items-center justify-center">
         <div class="flex h-fit w-fit items-center justify-center">
-            <img src="/mail.gif" class="h-[90px] w-[120px] logo" />
+            <img src="/mail.gif" class="lg:h-[290px] lg:w-[400px] logo" />
 
         </div>
-        <div class="flex">
+        <div class="flex text-red-700 start cursor-pointer">
+            <span class="mr-4">></span>
+            <span>press start</span>
 
         </div>
     </div>
 </template>
 
 <style scoped>
+.start {
+    text-shadow: white 1px 0px 2px;
+}
+
 .box {
-    border: 13px solid purple;
-    border-radius: 20px;
+    border: 20px solid #C80BDE;
+    border-radius: 40px;
 }
 </style>

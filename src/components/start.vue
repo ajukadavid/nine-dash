15736a65852eb935578558354a81e5a45db18326
ithiel883 <ajuka.zephiniah@gmail.com>
2023-06-30 03:47:43 +0100

    looks better

--- src/components/start.vue
+++ src/components/start.vue
@@ -5,10 +5,10 @@
 <template>
     <div class="box h-screen w-full gap-8 bg-black flex flex-col items-center justify-center">
         <div class="flex h-fit w-fit items-center justify-center">
-            <img src="/mail.gif" class="lg:h-[290px] lg:w-[400px] logo" />
+            <img src="/mail.gif" class="lg:h-[290px] lg:w-[500px] logo" />
 
         </div>
-        <div class="flex text-red-700 start cursor-pointer">
+        <div class="flex text-2xl text-red-700 start cursor-pointer">
             <span class="mr-4">></span>
             <span>press start</span>
 

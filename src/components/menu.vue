e7903bf16efd2678b8337430e1019a0494311a9e
ithiel883 <ajuka.zephiniah@gmail.com>
2023-07-03 16:27:42 +0100

    checking

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,17 +1,15 @@
 
 <template>
     <div class="flex justify-center bg-black h-screen pb-40">
-        <div class="flex-col">
-            <div>
-                <img src="/updated.gif" />
-            </div>
-            <div class="flex-col items-center justify-center w-full px-36">
+        <div class="flex-col h-screen bg-black">
+            <img src="/updated.gif" class="h-[400px]" />
+            <div class="flex-col items-center justify-center w-full bg-black px-36">
                 <div class="flex  items-center justify-center">
                     <img src="/skate.png" class="w-[150px] h-[200px]" alt="">
                     <img src="/shop.png" class="w-[150px] h-[200px]" alt="">
                     <img src="/music.png" class="w-[150px] h-[200px]" alt="">
                 </div>
-                <div class="flex  items-center justify-center mb-20">
+                <div class="flex  items-center justify-center mb-20 bg-black    ">
                     <img src="/contact.png" class="w-[150px] h-[200px]" alt="">
                     <img src="/subscribe.png" class="w-[150px] h-[200px]" alt="">
                     <img src="/about.png" class="w-[150px] h-[200px]" alt="">

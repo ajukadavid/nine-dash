e38c960efc11b1c3346a5e39cba0a506a77bbcc4
Ajuka David <ajuka.zephiniah@gmail.com>
2023-07-19 06:48:11 -0700

    Create shop.vue

---
+++ src/components/shop.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+    const linkUrl = "https://turndasix.company.site/";
+    window.location.replace(linkUrl);
+});
+</script>
+
+<template>loading...</template>

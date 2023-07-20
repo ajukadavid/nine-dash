e7f992a740aa088db785e21fd5c07754d2fea66b
Ajuka David <ajuka.zephiniah@gmail.com>
2023-07-20 05:55:00 +0000

    updated stuff

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -6,13 +6,13 @@ const $router = useRouter();
 
 <template>
     <div class="flex justify-center bg-black h-screen pb-40">
-        <div class="flex-col gap-10 h-screen bg-black">
+        <div class="flex-col  flex items-center h-screen bg-black">
             <img src="/updated.gif" class="h-[400px] mt-10" />
 
-            <div
-                class="flex-col items-center justify-center w-full bg-black px-36"
+            <div 
+                class="flex-col lg:flex hidden items-center justify-center w-fit bg-black px-36"
             >
-                <div class="flex items-center justify-center">
+                <div class="flex items-center cursor-pointer justify-center">
                     <img
                         @click="$router.push('/skate')"
                         src="/skate.png"
@@ -31,8 +31,6 @@ const $router = useRouter();
                         class="w-[150px] h-[200px]"
                         alt=""
                     />
-                </div>
-                <div class="flex items-center justify-center mb-20 bg-black">
                     <img
                         @click="$router.push('/contact')"
                         src="/contact.png"
@@ -52,7 +50,52 @@ const $router = useRouter();
                         alt=""
                     />
                 </div>
+               
             </div>
+            <div class="lg:hidden flex flex-col">
+                <div class="flex items-center cursor-pointer justify-center">
+                    <img
+                        @click="$router.push('/skate')"
+                        src="/skate.png"
+                        class="w-[150px] h-[200px] hover:w-[190px] hover:h-[220px]"
+                        alt=""
+                    />
+                    <img
+                        @click="$router.push('/shop')"
+                        src="/shop.png"
+                        class="w-[150px] h-[200px]"
+                        alt=""
+                    />
+                    <img
+                        @click="$router.push('/music')"
+                        src="/music.png"
+                        class="w-[150px] h-[200px]"
+                        alt=""
+                    />
+         
+                </div> 
+                  <div class="flex items-center cursor-pointer justify-center">
+                
+                    <img
+                        @click="$router.push('/contact')"
+                        src="/contact.png"
+                        class="w-[150px] h-[200px]"
+                        alt=""
+                    />
+                    <img
+                        @click="$router.push('/subscribe')"
+                        src="/subscribe.png"
+                        class="w-[150px] h-[200px]"
+                        alt=""
+                    />
+                    <img
+                        @click="$router.push('/about')"
+                        src="/about.png"
+                        class="w-[150px] h-[200px]"
+                        alt=""
+                    />
+                </div> 
+                </div>
         </div>
     </div>
 </template>

0bbf2a330369f0d109fb472cd3dd6dde4d0b05de
ithiel883 <ajuka.zephiniah@gmail.com>
2023-08-06 01:29:23 +0100

    added ish to skate page

--- src/components/skate.vue
+++ src/components/skate.vue
@@ -1,5 +1,8 @@
 <template>
-      <div class="flex bg-black text-red-700 flex-col h-screen items-center justify-center">
-        <span>Coming soon...</span>
+  <div class="flex bg-black text-red-700 flex-col h-screen items-center justify-center">
+    <div class="flex">
+      <iframe class="border-8 border-red-700" width="420" height="315" src="https://www.youtube.com/embed/rkQh6CKMQc8">
+      </iframe>
     </div>
+  </div>
 </template>

dff2e2471132bbb906e2986ee7d219f2089e172c
ithiel883 <ajuka.zephiniah@gmail.com>
2023-08-06 01:40:29 +0100

    added more vids

--- src/components/skate.vue
+++ src/components/skate.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="flex bg-black text-red-700 flex-col h-screen items-center justify-center">
+  <div class="flex bg-black gap-4 text-red-700 flex-col h-screen items-center pt-10">
     <div class="flex">
-      <iframe class="border-8 border-red-700" width="420" height="315" src="https://www.youtube.com/embed/rkQh6CKMQc8">
+      <iframe class="border-4 border-dotted p-2  border-red-700" width="200" height="215"
+        src="https://www.youtube.com/embed/rkQh6CKMQc8">
+      </iframe>
+    </div>
+    <div class="flex">
+      <iframe class="border-4 border-dotted p-2 border-red-700" width="200" height="215"
+        src="https://www.youtube.com/embed/Mkac8mQVNJ4">
       </iframe>
     </div>
   </div>

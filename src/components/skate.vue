f2fde7c6ee8007df65cbf2b01eadfa2d1c4c0419
ithiel883 <ajuka.zephiniah@gmail.com>
2023-08-09 20:23:25 +0100

    adding more images

--- src/components/skate.vue
+++ src/components/skate.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="flex bg-black gap-4 text-red-700 flex-col h-screen items-center pt-10">
-    <div class="flex">
+  <div class="p-10 lg:flex bg-black gap-4 justify-center text-red-700 h-screen items-center pt-10">
+    <div class="flex gap-4 flex-col">
       <iframe class="border-4 border-dotted p-2  border-red-700" width="200" height="215"
         src="https://www.youtube.com/embed/rkQh6CKMQc8">
       </iframe>
+      <span class="text-white text-sm">Lagos, Nigeria. 2022.</span>
+
     </div>
-    <div class="flex">
+    <div class="flex gap-4 flex-col">
       <iframe class="border-4 border-dotted p-2 border-red-700" width="200" height="215"
         src="https://www.youtube.com/embed/Mkac8mQVNJ4">
       </iframe>
+      <span class="text-white text-sm">Lagos, Nigeria. 2022.</span>
+
+    </div>
+    <div class="flex gap-4 flex-col">
+      <iframe class="border-4 border-dotted p-2 border-red-700" width="200" height="215"
+        src="https://www.youtube.com/embed/-J6k6zIDpnU">
+      </iframe>
+
+      <span class="text-white text-sm">God save 9ja</span>
     </div>
   </div>
 </template>
+
+https://youtu.be/-J6k6zIDpnU

e6a9bb6716726bd01dc3dd082d247fe48d5932c0
ithiel883 <ajuka.zephiniah@gmail.com>
2023-08-09 20:43:37 +0100

    added back btn

--- src/components/skate.vue
+++ src/components/skate.vue
@@ -1,28 +1,35 @@
 <template>
-  <div class="lg:flex bg-black flex flex-col gap-4 justify-center text-red-700 h-screen items-center pt-10">
-    <div class="flex items-center gap-4 flex-col">
-      <iframe class="border-4 border-dotted p-2  border-red-700" width="200" height="215"
-        src="https://www.youtube.com/embed/rkQh6CKMQc8">
-      </iframe>
-      <span class="text-white text-sm">Lagos, Nigeria. 2022.</span>
+  <div class=" bg-black">
 
+    <div @click="$router.back()" class="text-white mx-10 pt-10 cursor-pointer">
+      Back
     </div>
-    <div class="flex items-center gap-4 flex-col">
-      <iframe class="border-4 border-dotted p-2 border-red-700" width="200" height="215"
-        src="https://www.youtube.com/embed/Mkac8mQVNJ4">
-      </iframe>
-      <span class="text-white text-sm">Lagos, Nigeria. 2022.</span>
+    <div class="lg:flex lg:flex-row flex flex-col gap-4 justify-center text-red-700 h-screen items-center pt-10">
+      <div class="flex items-center gap-4 flex-col">
+        <iframe class="border-4 border-dotted p-2  border-red-700" width="200" height="215"
+          src="https://www.youtube.com/embed/rkQh6CKMQc8">
+        </iframe>
+        <span class="text-white text-sm">Lagos, Nigeria. 2022.</span>
 
-    </div>
+      </div>
+      <div class="flex items-center gap-4 flex-col">
+        <iframe class="border-4 border-dotted p-2 border-red-700" width="200" height="215"
+          src="https://www.youtube.com/embed/Mkac8mQVNJ4">
+        </iframe>
+        <span class="text-white text-sm">Lagos, Nigeria. 2022.</span>
 
-    <div class="flex gap-4 flex-col">
-      <iframe class="border-4 border-dotted p-2 border-red-700" width="200" height="215"
-        src="https://www.youtube.com/embed/-J6k6zIDpnU">
-      </iframe>
+      </div>
 
-      <span class="text-white text-sm">God save 9ja</span>
+      <div class="flex gap-4 flex-col">
+        <iframe class="border-4 border-dotted p-2 border-red-700" width="200" height="215"
+          src="https://www.youtube.com/embed/-J6k6zIDpnU">
+        </iframe>
+
+        <span class="text-white text-sm">God save 9ja</span>
+      </div>
+
+
     </div>
   </div>
 </template>
 
-https://youtu.be/-J6k6zIDpnU

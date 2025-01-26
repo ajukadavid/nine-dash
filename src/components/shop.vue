cc5a60b04b78a6d76cf53829d87f6f2258c5cbd1
Ajuka David <ajuka.zephiniah@gmail.com>
2025-01-26 21:21:21 +0100

    fix mobile

--- src/components/shop.vue
+++ src/components/shop.vue
@@ -9,28 +9,30 @@
 
 <template>
     <div class="text-red-700 w-full h-screen bg-black">
-        <div class="px-8 pt-6">
-            <span class="text-red-500 text-2xl">SHOP</span>
+        <div class="px-4 sm:px-8 pt-6">
+            <span class="text-red-500 text-xl sm:text-2xl">SHOP</span>
         </div>
-        <div class="w-full space-y-10 h-[calc(100%-4rem)] flex flex-col items-center justify-center">
-            <span class="text-red-500 text-2xl"> Select your location </span>
-            <a href="https://turndasix.company.site/" target="_blank" class="cursor-pointer">
-                <div class="flex space-x-10 justify-center items-center">
-                    <span class="text-red-500 text-2xl"> > Nigeria </span>
-                    <img src="/naija.png" class="logo-img w-16 h-10 cursor-pointer" />
+        <div class="w-full space-y-6 sm:space-y-10 h-[calc(100%-4rem)] flex flex-col items-center justify-center px-4 sm:px-8">
+            <span class="text-red-500 text-xl sm:text-2xl text-center"> Select your location </span>
+            
+            <a href="https://turndasix.company.site/" target="_blank" class="w-full sm:w-auto">
+                <div class="flex space-x-4 sm:space-x-10 justify-center items-center">
+                    <span class="text-red-500 text-lg sm:text-2xl"> > Nigeria </span>
+                    <img src="/naija.png" class="logo-img w-12 sm:w-16 h-8 sm:h-10" />
                 </div>
             </a>
-            <a href="https://turndasixcnda.company.site/" target="_blank" class="cursor-pointer">
-                <div class="flex space-x-10 justify-center items-center">
-                    <span class="text-red-500 text-2xl"> > Canada </span>
-                    <img src="/cana.jpg" class="logo-img w-16 h-10 cursor-pointer" />
+            
+            <a href="https://turndasixcnda.company.site/" target="_blank" class="w-full sm:w-auto">
+                <div class="flex space-x-4 sm:space-x-10 justify-center items-center">
+                    <span class="text-red-500 text-lg sm:text-2xl"> > Canada </span>
+                    <img src="/cana.jpg" class="logo-img w-12 sm:w-16 h-8 sm:h-10" />
                 </div>
             </a>
          
-            <a href="https://turndasix.company.site/" target="_blank" class="cursor-pointer">
-                <div class="flex space-x-10 justify-center items-center">
-                    <span class="text-red-500 text-2xl"> > Rest of the World </span>
-                    <img src="/globe.png" class="logo-img w-16 h-16 cursor-pointer" />
+            <a href="https://turndasix.company.site/" target="_blank" class="w-full sm:w-auto">
+                <div class="flex space-x-4 sm:space-x-10 justify-center items-center">
+                    <span class="text-red-500 text-lg sm:text-2xl"> > Rest of the World </span>
+                    <img src="/globe.png" class="logo-img w-12 sm:w-16 h-12 sm:h-16" />
                 </div>
             </a>
         </div>

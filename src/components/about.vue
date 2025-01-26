135c7ee62e11bbb3c9fbcbfd4dcb684289b0bd1d
Ajuka David <ajuka.zephiniah@gmail.com>
2025-01-26 21:54:24 +0100

    added about

--- src/components/about.vue
+++ src/components/about.vue
@@ -7,7 +7,20 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="flex bg-black text-red-700 flex-col h-screen items-center justify-center">
-        <span>Coming soon...</span>
+    <div class="flex bg-black text-red-700 flex-col h-screen items-center px-2 sm:px-8 justify-center">
+       
+        <span class="text-red-700 text-xs text-left">
+
+“I don't care what society thinks <br>
+They're nothing anyway <br>
+They're no better than me <br>
+Out there you just have to fit into a pattern that <br/> somebody's already laid out for you <br/>
+Life we live, you have to set your own patterns, <br/> your own ideals <br>
+You have to handle the whole job yourself”
+
+</span>
+<span class="text-red-700 mt-10">
+    turndasix
+</span>
     </div>
 </template>
